983531b43febc5f3724111865970d132c7c25b11
MiguelOrtega16 <miguel.ortega16@hotmail.com>
2026-05-09 13:58:42 -0500

    - Updated middleware with cron routes

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -36,7 +36,7 @@ export async function updateSession(request: NextRequest) {
 
   const { data: { user } } = await supabase.auth.getUser()
 
-  const publicPaths = ['/delete-account']
+  const publicPaths = ['/delete-account', '/api/cron/', '/api/send-push']
   if (!user && !request.nextUrl.pathname.startsWith('/auth') && !publicPaths.some(p => request.nextUrl.pathname.startsWith(p))) {
     const url = request.nextUrl.clone()
     url.pathname = '/auth/login'
